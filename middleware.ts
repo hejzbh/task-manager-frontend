34853b11417acd7a4b5eab3681bba1fef684c2d6
hejzbh <hazimba123@gmail.com>
2025-02-26 21:53:59 +0100

    debug

--- middleware.ts
+++ middleware.ts
@@ -13,21 +13,21 @@ export default async function middleware(req: NextRequest) {
 
   // 3) Get user session
   const session = await useSession();
-  /** 
+
   // 4) If route is protected but there's no session
   if (isProtectedRoute && !session) {
     // Redirect user to the login
     return NextResponse.redirect(new URL("/auth/login", req.url));
   }
 
-  // 5) Check is auth route
+  /**  // 5) Check is auth route
   const isAuthRoute = pathname.startsWith(ROUTES.AUTH);
 
   // 6) If User trying to access /auth route even he's logged in
   if (isAuthRoute && session) {
     // Redirect user to the dashboard
     return NextResponse.redirect(new URL(ROUTES.DASHBOARD, req.url));
-  } */
+  }  */
 
   // If everything is okay, proceed user to the page
   return NextResponse.next();

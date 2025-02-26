c90d78a4c148a9430928279fc7ed8dda02ef2528
hejzbh <hazimba123@gmail.com>
2025-02-26 22:18:23 +0100

    edit

--- modules/auth/utils/getUserByToken.ts
+++ modules/auth/utils/getUserByToken.ts
@@ -5,16 +5,21 @@ const cache = new Map();
 let userPromise: Promise<any> | null = null;
 
 export const getUserByToken = async (token: string) => {
+  console.log(`Token : ${token}`);
   if (!token) return null;
 
+  console.log(`UserPromise : ${userPromise !== null}`);
   if (userPromise) return userPromise;
 
   const cachedUser = cache.get(token);
+  console.log(`CachedUser : ${cachedUser}`);
+
   if (cachedUser) return cachedUser;
 
   userPromise = axiosInstance
     .get("/auth/profile", { headers: { Authorization: `Bearer ${token}` } })
     .then((response) => {
+      console.log(`✅✅✅ ${response.data.data} Uspjesno!!`);
       const { user } = response?.data?.data || {};
       if (!user) throw new Error("User not found");
 

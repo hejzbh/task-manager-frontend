aa20797c77ddb93b22da271bbbb2516bb2bb4970
hejzbh <hazimba123@gmail.com>
2025-02-26 22:13:11 +0100

    edit

--- modules/auth/utils/getUserByToken.ts
+++ modules/auth/utils/getUserByToken.ts
@@ -1,13 +1,8 @@
 "use server";
 import { axiosInstance } from "@/lib/axios";
 
-// For developers from company that I'm applying for (explanation):
-
-// In-memory cache for storing user data per token (Prevent unecessary api requests that MIDDLEWARE can cause)
-// I want to mention this is good solution for fronnted (to keep somethig in memory), but we should avoid this on BACKEND!! Because it can cause MEMORY LEAK
-
 const cache = new Map();
-let userPromise: any = null;
+let userPromise: Promise<any> | null = null;
 
 export const getUserByToken = async (token: string) => {
   if (!token) return null;
@@ -15,11 +10,7 @@ export const getUserByToken = async (token: string) => {
   if (userPromise) return userPromise;
 
   const cachedUser = cache.get(token);
-
   if (cachedUser) return cachedUser;
-
-  // Exlpanation: Because this unction is used in middleware that runs for each request (maybe 10times in 1 second) we dont wwant to make an 10+ api calls,
-  // "userPromise" ensures that simultaneous requests for the same token wait for the same API response, improving performance and reducing backend load
 
   userPromise = axiosInstance
     .get("/auth/profile", { headers: { Authorization: `Bearer ${token}` } })
@@ -27,15 +18,16 @@ export const getUserByToken = async (token: string) => {
       const { user } = response?.data?.data || {};
       if (!user) throw new Error("User not found");
 
+      cache.set(token, user);
       return user;
     })
-    .catch(() => null)
+    .catch((err) => {
+      console.error(err);
+      return null;
+    })
     .finally(() => {
       userPromise = null;
     });
-
-  cache.clear(); // Someone can spam different tokens and fill memory (we need to care about details :))
-  cache.set(token, userPromise);
 
   return userPromise;
 };
